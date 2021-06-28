1feb502b7245d60995acc32690d5456bf782e56b
JackXu <jackxu1999@hotmail.com>
2021-06-28 16:16:11 +0800

    country data with flag

---
+++ src/components/DataTitle.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="text-center">
+        
+        <h2 class="text-3xl font-bold"><span :class=concatCountry></span> {{text}}</h2>
+        <div class="text-2xl mt-4 mb-10">{{timestamp}}</div>
+    </div>
+</template>
+
+<script>
+import moment from 'moment'
+import 'flag-icon-css/css/flag-icon.css'
+
+export default {
+    name: 'DateTitle',
+    props: ['text', 'dataDate', 'countryCode'],
+
+    computed: {
+        timestamp: function() {
+            return moment(this.dataDate).format('MMMM Do YYYY, h:mm:ss a')
+        },
+        concatCountry: function() {
+            return 'flag-icon flag-icon-' + this.countryCode;
+        }
+    },
+}
+</script>
